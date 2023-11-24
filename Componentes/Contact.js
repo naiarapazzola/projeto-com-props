685fc130d133d3204000a8f051f38caa5a6751fc
Naiara Pazzola <86858169+naiarapazzola@users.noreply.github.com>
2023-11-23 21:35:12 -0300

    componentes e estilos
    adicionando componentes e estilo para praticar props, useState, e outras ferramentas

---
+++ Componentes/Contact.js
@@ -0,0 +1,9 @@
+const Contato = () => {
+    return(
+        <div className="App-link">
+            <a href="mailto:naiarapazzola@gmail.com" >Entre em contato</a>
+        </div>
+    );
+}
+
+export default Contato;
